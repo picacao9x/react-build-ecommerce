2c01a2894a9dfd0b0d150b4d1b448173ee952f4a
BanhMi <picacao9x@gmail.com>
2025-10-30 21:26:04 +0700

    init

--- src/components/ProductCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function ProductCard ({ name,price,image }) {
-    return(
-        <div className="product-card">
-            <img src={image} alt={name} />
-            <h3>{name}</h3>
-            <p>${price}</p>
-        </div>  
-    )
-}
--- src/pages/Cart.jsx
+++
@@ -1,8 +0,0 @@
-export default function Cart() {
-  return (
-    <div className="container">
-      <h1>Cart Page</h1>
-      <p>This is the cart page (coming soon).</p>
-    </div>
-  );
-}
